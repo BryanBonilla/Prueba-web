d26d6968f6eff91139722bbf049d81b725eab799
BryanBonilla <bbonilla04@alumnos.uaq.mx>
2025-02-11 09:35:02 -0600

    fix: HomeController

--- Prueba/obj/Debug/net9.0/Prueba.AssemblyInfo.cs
+++ Prueba/obj/Debug/net9.0/Prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83263029209ae3f75a73884d65fe8b984b64a8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39021b5595e6eba63582c03d91f3571b89dd4453")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
